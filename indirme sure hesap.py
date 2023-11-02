09ef4736905531c978933e660413a4c7d662568d
Barış Keskin <100820351+keserbaros@users.noreply.github.com>
2023-11-03 01:21:37 +0300

    son_boyut hata düzeltme, açıklayıcı metinler

--- indirme sure hesap.py
+++ indirme sure hesap.py
@@ -14,8 +14,9 @@ def Yanitlayici(mesaj):
 
 while (1):
     print('Sırasıyla "toplam boyutu, ,inen boyutu, indirme hızını" birimleriyle birlikte giriniz:')
-    print("Örn:20.5 GB 500 mb 110 Mb")
-    print("!Sadece indirme hızın biriminde büyük küçük harf duyarlı!")
+    print("Örn:120.5 GB 500 mb 1 GB")
+    print("!Sadece indirme hızın biriminde büyük küçük harf duyarlı! (Örn: MB, Mb)")
+    print("Yukarı ok yönü tuşuyla bir önceki değerlerinizi görebilirsiniz")
     veri = str(input())
     ilk_boyut = veri.lower().split()
     hiz = veri.split()
@@ -27,7 +28,7 @@ while (1):
         elif ilk_boyut[1] == "mb" and ilk_boyut[3] == "mb":
             son_boyut = float(ilk_boyut[0]) - float(ilk_boyut[2])
         elif ilk_boyut[1] == "mb" and ilk_boyut[3] == "gb":
-            raise Inecek_Boyut
+            son_boyut = float(ilk_boyut[0]) - float(ilk_boyut[2])*1024
         else:
             print("Program veri boyutunu GB veya MB şeklinde desteklemekte")
         if son_boyut < 0:
@@ -52,18 +53,18 @@ while (1):
     except (IndexError, TypeError, NameError ):  # programda ilk doğru ifade girip sonra hızın boyutunu yanlış girince hatayı göstermiyor. Önceki. sonucu gösteriyor
         mesaj = "Boyutu hatalı girdiniz"
         Yanitlayici(mesaj)
-        input()
+        input("Devam etmek için Enter'a basın.")
 
     except (ZeroDivisionError):
         mesaj = "İndirme hızı 0 olamaz"
         Yanitlayici(mesaj)
-        input()
+        input("Devam etmek için Enter'a basın.")
     
     except ValueError:
         mesaj = "Sayıyı hatalı girdiniz"
         Yanitlayici(mesaj)
-        input()
+        input("Devam etmek için Enter'a basın.")
 
     except Inecek_Boyut as nesne:
         Yanitlayici(nesne.mesaj)
-        input()
+        input("Devam etmek için Enter'a basın.  ")
